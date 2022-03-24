687ef4ea48ca6208805b04f94d35074d98097c03
Daniel Nelson <dan.s.nelson@gmail.com>
2022-03-24 16:32:15 -0400

    finished README, cleaned up issues with retrieval from local storage, staged final deployment

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Work-Day-Scheduler
+
+## Description
+
+In this project I was tasked with building a simple hour-by-hour scheduler for standard work day hours. I had to present the updated current date, allow users to input events into an hour block, save those events in local storage for page reloads, and to color code hour blocks by status as a past, present, or future entity.
+
+The primary problems I faced involved employing jquery for DOM traversal, event handling, and search/iteration tasks instead of the regular Javascript syntax for those items. I also had to demonstrate a knowledge of how to access third-party APIs (mainly MomentJS for date and time) to bring the scheduler to appropriate functionality.
+
+The following image is a screen shot from the day scheduler:
+
+![image](./assets/images/Screen%20Shot%202022-03-24%20at%204.23.17%20PM.jpeg)
+
+## Installation
+
+Github repository for this project can be found at: https://github.com/dannelson714/Work-Day-Scheduler
+
+URL of the deployed application: https://dannelson714.github.io/Work-Day-Scheduler
+
+## Credits
+
+I can be found at https://github.com/dannelson714
+
+I would like to credit:
+Leif Hetland: https://github.com/leifehetland &
+Laura Cole: https://github.com/LauraCole1900
+for their help in making this project come to fruition.
+
+## License
+
+MIT License
+
+Copyright (c) [2022] [Daniel Stephen Nelson]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,20 @@
+//Creating the daily date
 var today = moment();
 $("#currentDay").text("Today is " +today.format("dddd, MMMM Do"));
-var hours = [9,10,11,12,13,14,15,16,17]
 
+//Retrieving filled in schedule items from local storage
+$('#09').val(JSON.parse(localStorage.getItem('btn9')));
+$('#10').val(JSON.parse(localStorage.getItem('btn10')));
+$('#11').val(JSON.parse(localStorage.getItem('btn11')));
+$('#12').val(JSON.parse(localStorage.getItem('btn12')));
+$('#13').val(JSON.parse(localStorage.getItem('btn13')));
+$('#14').val(JSON.parse(localStorage.getItem('btn14')));
+$('#15').val(JSON.parse(localStorage.getItem('btn15')));
+$('#16').val(JSON.parse(localStorage.getItem('btn16')));
+$('#17').val(JSON.parse(localStorage.getItem('btn17')));
+
+//Establishing the proper color coding for each hour block
 timeCode = $(".timeSlot");
-
 
 function timeColorCode() {
     currentHour = moment().format("H");
@@ -23,25 +34,59 @@ function timeColorCode() {
     })
 }
 
-
 timeColorCode();
 
-$('#09').val(localStorage.getItem('btn9'))
-
-
-$('#1').on("click", function() {
+//Button functionality for each button for each hour
+$('#0').on("click", function() {
     toStore = $('#09').val();
     console.log(toStore);
     localStorage.setItem("btn9", JSON.stringify(toStore))
 });
 
-// scheduleList = ["","","","","","","",""]
+$('#1').on("click", function() {
+    toStore = $('#10').val();
+    console.log(toStore);
+    localStorage.setItem("btn10", JSON.stringify(toStore))
+});
 
-// var saveBtn = $('.saveBtn');
-// console.log(saveBtn);
+$('#2').on("click", function() {
+    toStore = $('#11').val();
+    console.log(toStore);
+    localStorage.setItem("btn11", JSON.stringify(toStore))
+});
 
-// saveBtn.on('click', function() {
-//     var toStore = Event.target;
-//     // siblings().attr("id").val();
-//     console.log(toStore);
-// });
+$('#3').on("click", function() {
+    toStore = $('#12').val();
+    console.log(toStore);
+    localStorage.setItem("btn12", JSON.stringify(toStore))
+});
+
+$('#4').on("click", function() {
+    toStore = $('#13').val();
+    console.log(toStore);
+    localStorage.setItem("btn13", JSON.stringify(toStore))
+});
+
+$('#5').on("click", function() {
+    toStore = $('#14').val();
+    console.log(toStore);
+    localStorage.setItem("btn14", JSON.stringify(toStore))
+});
+
+$('#6').on("click", function() {
+    toStore = $('#15').val();
+    console.log(toStore);
+    localStorage.setItem("btn15", JSON.stringify(toStore))
+});
+
+$('#7').on("click", function() {
+    toStore = $('#16').val();
+    console.log(toStore);
+    localStorage.setItem("btn16", JSON.stringify(toStore))
+});
+
+$('#8').on("click", function() {
+    toStore = $('#17').val();
+    console.log(toStore);
+    localStorage.setItem("btn17", JSON.stringify(toStore))
+});
